f9679f9c2e4c2e52f7f3583d2825410697d29b85
Luisao <luisao64@hotmail.com>
2024-05-19 17:16:40 -0500

    Nuevo

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-           default='postgresql://postgres:postgres@localhost/postgres',
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
