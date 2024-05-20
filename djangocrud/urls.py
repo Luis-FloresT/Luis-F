d764a3ad5ffcf8bead8eb132af2e46fc90d69470
Luisao <luisao64@hotmail.com>
2024-05-19 21:12:42 -0500

    New

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -1,7 +1,8 @@
-"""djangocrud URL Configuration
+"""
+URL configuration for djangocrud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,16 +18,20 @@ from django.contrib import admin
 from django.urls import path
 from tasks import views
 
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
-    path('logout/', views.signout, name='logout'),
-    path('signin/', views.signin, name='signin'),
-    path('create_task/', views.create_task, name='create_task'),
-    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
-    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
-    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    path('',views.home, name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('tasks/',views.tasks,name='tasks'),
+    path('tasks_completed/',views.tasks_completed,name='tasks_completed'),
+    path('tasks/create/',views.create_task,name='create_task'),
+    path('tasks/<int:task_id>/',views.task_detail,name='task_detail'),
+    path('tasks/<int:task_id>/complete',views.complete_task,name='complete_task'),
+    path('tasks/<int:task_id>/delete',views.delete_task,name='delete_task'),
+    path('logout/',views.signout,name='logout'),
+    path('signin/',views.signin,name='signin'),
 ]
