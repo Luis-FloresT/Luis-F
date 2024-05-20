a2130439c74aac0788a0b27e8283c0d6ebb01710
Luisao <luisao64@hotmail.com>
2024-05-19 19:00:36 -0500

    nuevo

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# exit on error
+set -o errexit
+# poetry install
 pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
 
